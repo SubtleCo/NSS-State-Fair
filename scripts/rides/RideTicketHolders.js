f555e8a6780b42b24100d3c237075ae730f61b4c
Alex Martin <alex@subtleco.co>
2021-01-26 08:31:20 -0600

    Added rider display

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -2,7 +2,9 @@ const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+    eventHub.addEventListener("rideTicketPurchased", e => {
+        const targetElement = document.querySelector(".rides")
+         targetElement.innerHTML += `
+         <div class="person rider"></div>`
     })
 }
-
